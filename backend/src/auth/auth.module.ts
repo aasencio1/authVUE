25f95cf42f7a290fcecb975b9c57693ed88c12d3
Jose Asencio Perez <asdrubal.asencio@gmail.com>
2025-08-21 15:59:34 -0400

    testing github action job backend-unit 2

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -19,7 +19,7 @@ import { JwtAuthGuard } from './jwt-auth.guard';
         const secret = config.get<string>('JWT_SECRET');
         console.log('JwtModule factory: JWT_SECRET present?', !!secret); // <-- trazas
         return {
-          secret, // SI es undefined, aquí verás 'false'
+          secret, // SI ES undefined, aquí verás 'false'
           signOptions: { expiresIn: config.get<string>('JWT_EXPIRES') || '1h' },
         };
       },
