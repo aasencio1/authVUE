4ec9cb1df61bea6a57fecfcf5bdd742ad33a7223
Jose Asencio Perez <asdrubal.asencio@gmail.com>
2025-09-10 09:59:39 -0400

    test hooks 2

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   );
   console.log('CALLBACK?', process.env.GOOGLE_CALLBACK_URL);
   console.log('Entered');
-  //  await app.listen(process.env.PORT ?? 3000);
+  //  await app.listen(process.env.PORT ?? 3050);
   app.enableCors({
     origin: process.env.FRONTEND_URL || 'http://localhost:5173',
     credentials: true,
